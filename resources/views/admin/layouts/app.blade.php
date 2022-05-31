66ca643d38324e52ae763e2c6247c705c767ceb1
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-05-31 20:17:00 +0430

    create dashboard template / import dasboard assets

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,95 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Starter page | Appzia - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset(' assets/images/favicon.ico')}}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<!-- <body data-layout="horizontal" data-topbar="light"> -->
+
+<!-- Begin page -->
+<div id="layout-wrapper">
+
+
+    @include('admin.layouts.partials.navbar')
+
+    <!-- ========== Left Sidebar Start ========== -->
+
+    @include('admin.layouts.partials.vertical_menu')
+    <!-- Left Sidebar End -->
+
+
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+        <div class="page-content">
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                            <h4 class="mb-sm-0">Starter page</h4>
+
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Utility</a></li>
+                                    <li class="breadcrumb-item active">Starter page</li>
+                                </ol>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- end page title -->
+                @yield('content')
+            </div> <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+
+        @include('admin.layouts.partials.footer')
+
+    </div>
+    <!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+
+<!-- Right Sidebar -->
+
+<!-- /Right-bar -->
+
+<!-- Right bar overlay-->
+
+
+<!-- JAVASCRIPT -->
+<script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
+
+</body>
+</html>
---
+++ resources/views/admin/layouts/partials/vertical_menu.blade.php
@@ -0,0 +1,69 @@
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Menu</li>
+
+                <li>
+                    <a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/index.html" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">3</span>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/calendar.html" class=" waves-effect">
+                        <i class="ri-calendar-2-line"></i>
+                        <span>Calendar</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/apps-chat.html" class=" waves-effect">
+                        <i class="ri-chat-1-line"></i>
+                        <span>Chat</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-layout-3-line"></i>
+                        <span>Layouts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow">Vertical</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-light-sidebar.html">Light Sidebar</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-compact-sidebar.html">Compact Sidebar</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-icon-sidebar.html">Icon Sidebar</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-boxed.html">Boxed Layout</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-preloader.html">Preloader</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-colored-sidebar.html">Colored Sidebar</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-horizontal.html">Horizontal</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-hori-topbar-light.html">Topbar light</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-hori-boxed-width.html">Boxed width</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-hori-preloader.html">Preloader</a></li>
+                                <li><a href="web-template/appziaadmin-20/appziaadmin-20/Appzia_v2.0/Admin/dist/layouts-hori-colored-header.html">Colored Header</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
