f3075a35a914ae9335db3944f8c2496bb5cab068
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-06-02 10:17:44 +0430

    write some method doc

--- app/Http/Controllers/Admin/ColorController.php
+++ app/Http/Controllers/Admin/ColorController.php
@@ -8,12 +8,21 @@ use App\Http\Requests\StoreColorRequest;
 
 class ColorController extends Controller
 {
+    /**
+     * return  all data to view
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         $colors = ColorService::getWithPaginate();
         return view('admin.colors.index', compact('colors'));
     }
 
+    /**
+     * store new color into db
+     * @param StoreColorRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(StoreColorRequest $request)
     {
         ColorService::create($request);
--- app/Http/Controllers/Admin/Services/ColorService.php
+++ app/Http/Controllers/Admin/Services/ColorService.php
@@ -8,15 +8,23 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ColorService extends Controller
 {
-    //
+    /**
+     * return data with pagination
+     * @param null $perPage
+     * @return mixed
+     */
     public static function getWithPaginate($perPage = null)
     {
         return Color::paginate($perPage ?? config('shop.perPage'));
     }
 
+    /**
+     * store new data in db
+     * @param Request $request
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
+     */
     public static function create(Request $request)
     {
-
         return Color::query()
                     ->create($request->toArray());
     }
